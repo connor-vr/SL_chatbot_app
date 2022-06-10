19665aeb50ccbf6ea922b40885181b3bb2fab84c
Connorvr <connor@brightagi.com>
2022-06-10 14:57:24 +1000

    Update steamlit_host.py

--- steamlit_host.py
+++ steamlit_host.py
@@ -21,27 +21,23 @@ st.header("WiseBot")
 #st.markdown("[Github](https://github.com/---)")
 
 if 'generated' not in st.session_state:
-    st.session_state['generated'] = [' ']
+    st.session_state['generated'] = []
 
 if 'past' not in st.session_state:
-    st.session_state['past'] = [' ']
-    
-for i in range(len(st.session_state['generated'])-1):
-    message(st.session_state["generated"][i], key=str(i))
-    message(st.session_state['past'][i], is_user=True, key=str(i) + '_user')
+    st.session_state['past'] = []
 
 def query(payload):
 	response = requests.post(API_URL, headers=headers, json=payload)
 	return response.json()
 
 def get_text():
-    input_text = st.text_input("You: ")
+    input_text = st.text_input("You: ","Hello, how are you?", key="input")
     return input_text 
 
 
 user_input = get_text()
 
-if user_input and len(user_input)>0:
+if user_input:
     payload = {
         "inputs": {
             "past_user_inputs": st.session_state.past,
@@ -70,9 +66,9 @@ if user_input and len(user_input)>0:
 
     st.session_state.past.append(user_input)
     st.session_state.generated.append(response["generated_text"])
-    # max_context = 4
-    # if len(st.session_state.past) > max_context:
-    #     st.session_state['past'] = st.session_state.past[-max_context:]
-    # if len(st.session_state.generated) > max_context:
-    #     st.session_state['generated'] = st.session_state.generated[-max_context:]
-        
+
+if st.session_state['generated']:
+
+    for i in range(len(st.session_state['generated'])-1, -1, -1):
+        message(st.session_state["generated"][i], key=str(i))
+        message(st.session_state['past'][i], is_user=True, key=str(i) + '_user')
